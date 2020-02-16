f30da263d671b0aa53f210847d05763ac3057803
AhMaDeLMoMsNi <ahmad.mom.20000@gmail.com>
2020-02-16 22:08:04 +0200

    Initial rescources

--- ImageCombine.py
+++ ImageCombine.py
@@ -1,5 +1,4 @@
 from PIL import Image, ImageDraw, ImageFont
-class ImageCombine:
+class ImageCombine(Image.Image) :
     def __init__(self,date,weather,backgroud,foreground):
-        
-
+        pass
